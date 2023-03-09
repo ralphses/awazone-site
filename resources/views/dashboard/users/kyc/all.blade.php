4042d77a103add44f9f02721de4d7b3c299bfc96
Raphael Eze <eze.raph@gmail.com>
2023-03-09 02:26:05 +0100

    fixed kyc doc type issue

--- resources/views/dashboard/users/kyc/all.blade.php
+++ resources/views/dashboard/users/kyc/all.blade.php
@@ -1,3 +1,3 @@
 <x-app-layout title="Awazone | Dashboard - KYC">
-    <x-dashboard.users.kyc.all :kycs="$kycs" :pages="$pages" />
+    <x-dashboard.users.kyc.all :kycs="$kycs" :pages="$pages" :types="$types" />
 </x-guest-layout>
