a8315c99d49d43395b609a34e84d80f3b9c798fa
Raphael Eze <eze.raph@gmail.com>
2023-03-26 12:18:32 +0100

    fixed production bugs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(200);
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
+        
     }
 }
